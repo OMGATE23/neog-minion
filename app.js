df0790bb21e965f6847cd6760e781c3d298d805a
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-08-25 19:20:43 +0530

    feat: added translation API and error handling

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ var outputDiv = document.querySelector("#output-div")
 
 btnTranslate.addEventListener("click", clickHandler)
 
+var URL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getURL(text){
+    let mainURL = URL + "?text=" + text
+    return URL + "?text= " + text
+}
+
+function errorHandler(error){
+    outputDiv.innerHTML = "Opps, seems like you reached your limit"
+    outputDiv.getElementsByClassName.fontWeight = "bold"
+}
+
 
 function clickHandler(){
-     
+    let text = txtInput.value
+    fetch(getURL(text))
+    .then(resp => resp.json())
+    .then(json => getTranslation((json.contents.translated)))
+    .catch(errorHandler)
+}
+
+
+function getTranslation(input){
+    outputDiv.innerHTML = input
 }
